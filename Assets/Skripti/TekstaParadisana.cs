306b38c72331c7de83c747b21a578055a78e5f98
Narvils <gustavs.gn@gmail.com>
2022-02-16 14:14:56 +0200

    Darbibas ar tekstu

---
+++ Assets/Skripti/TekstaParadisana.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TekstaParadisana : MonoBehaviour {	
+    //Uzglabā tekstu, kurš paņemts no input lauka
+	public string teksts;
+	//Lauks no kura es tekstu paņemšu
+	public GameObject ievadeslauks;
+	//Teksta objekts, kur attēlot mainīgā teksts saturu
+	public GameObject tekstaAttelosanasLauks;
+	public void darbibasArTekstu(){
+		teksts = ievadeslauks.GetComponent<Text> ().text;
+		tekstaAttelosanasLauks.GetComponent<Text>().text = "Sveiks "+teksts.ToUpper()+"!";
+	}
+		
+}
