0aae06a9a31991b520db3cb5aa7d4d2f75882b12
Narvils <gustavs.gn@gmail.com>
2022-02-22 15:24:09 +0200

    Pabeigtais beans

---
+++ Assets/Skripti/KursorsUzAttela.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KursorsUzAttela : MonoBehaviour {
+	public AudioSource skanasAvots;
+	public AudioClip skanaKoAtskenot;
+	public void  uzbiditsUzAttela(){
+		skanasAvots.PlayOneShot (skanaKoAtskenot);
+	}
+	public void  nobiditsUzAttela(){
+		skanasAvots.Stop ();
+	}
+}
